becf7551a03a4513e7f110ee267cbf84bb23ea52
iaraelise <iara.elise@escolar.ifrn.edu.br>
2026-04-13 17:54:37 -0300

    feat: resolve a questão 1095

---
+++ lista-1/Q1095.java
@@ -0,0 +1,18 @@
+public class Q1095 {
+    public static void main(String[] args) {
+        
+        // DECLARAÇÃO DE VARIÁVEIS
+
+        int i = 1;
+        int j = 60;
+
+        while (j > 0) {
+            i += 3;
+            j -= 5;
+
+            System.out.print("I = " + i);
+            System.out.println("J = " + j);
+        }
+
+    }
+}
